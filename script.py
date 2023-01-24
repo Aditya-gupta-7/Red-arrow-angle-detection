957a00ae9f95d299466834edb6bc15d4de329921
Aditya Gupta <68244807+Aditya-gupta-7@users.noreply.github.com>
2023-01-24 10:20:12 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,38 +1,11 @@
-### DOCUMENTATION GUIDE
-### ---
-### All Important Info is in Pound-Sign Comments (#)
-### All unused code that was used in previous versions but is obsolete now is
-### in Quotation Mark comments (''')
-### ---
-### Press the Q key on the keyboard to exit the script when its running
-### ---
-### Keshav Nath - 2K20/ME/123
-
 import cv2
 import numpy as np
 
-'''
-pinky=np.uint8([[[255,115,160]]])
-hsv_pinky=cv2.cvtColor(pinky,cv2.COLOR_BGR2HSV)
-print(hsv_pinky)
-'''
 
 #Lower and Upper limits of Hue for Red Color Detection
 lower=np.array([170, 50, 50],np.uint8)
 higher=np.array([180, 255, 255],np.uint8)
 
-'''
-base=np.array([0,0,100],np.uint8)
-'''
-
-'''
-#Taking template of arrow template
-base_arrow=cv2.imread("arrow.jpg",0)
-ret, thresh = cv2.threshold(base_arrow, 255, 255, 255)
-cnts,h = cv2.findContours(thresh,2,1)
-arrow_template=cnts[0]
-#cv2.imshow("Lmao",base_arrow)
-'''
 
 #A Kernel - 7x7 Pixels was found to be the ideal size for me - Used for Morphology
 kernel = np.ones((7,7), np.uint8)
@@ -80,38 +53,17 @@ while (True):
         #Only dealing with the larger contours, so that
         #any accidental noise is not considered for the angle or the circle
         if(area > 3000): 
-            '''
-            rect = cv2.minAreaRect(contour)
-            box = cv2.boxPoints(rect)
-            box = np.int0(box)
-            frame = cv2.drawContours(frame,[box],0,(0,0,255),2)
-            '''
-
             #Drawing the circle around the detected arrow
             (x,y),radius = cv2.minEnclosingCircle(contour)
             center = (int(x),int(y))
             radius = int(radius)
             cv2.circle(frame,center,radius,(0,255,0),2)
 
-            ''' 
-            frame = cv2.rectangle(frame, (xr, yr),  
-                                       (xr + wr, yr + hr),  
-                                       (255, 0, ), 2) 
-            '''
-
             #Splitting into Left and Right on the basis of centroid of Arrow
             left=mask[0:h, 0:int(x)]
             right=mask[0:h, int(x):w]
             arealeft=int()
             arearight=int()
-
-            '''
-            xr, yr, wr, hr = cv2.boundingRect(contour)
-            vid_arrow=mask[yr:yr+hr,xr:xr+wr]
-            cv2.imshow("",vid_arrow)
-            ccnt,h = cv2.findContours(vid_arrow,2,1)
-            vid_arrow=ccnt[0]
-            '''
 
             #Finding area of contours on left and right side
             lc, lh = cv2.findContours(left, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -133,25 +85,7 @@ while (True):
             #Writing angle on the center of the Arrow
             cv2.putText(frame, f"A={int(angle)}",(x0,y0), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255))
 
-            '''
-            rett = cv2.matchShapes(arrow_template,vid_arrow,1,0.0)
-            print(rett)
-            '''
-
-            '''
-            print(f"X={cords[0]}\nY={cords[1]}\nAngle={angle}\n")
-            print(axes)
-            '''
-
-            '''
-            rows,cols = frame.shape[:2]
-            [vx,vy,x0,y0] = cv2.fitLine(contour, cv2.DIST_L2,0,0.01,0.01)
-            lefty = int((-x0*vy/vx) + y0)
-            righty = int(((cols-x0)*vy/vx)+y0)
-            cv2.putText(frame, f"LY={lefty}",(x,y+20), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0))
-            cv2.putText(frame, f"RY={righty}",(x,y+40), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0))
-            #frame = cv2.line(frame,(cols-1,righty),(0,lefty),(0,255,0),2)
-            '''
+            
             
     #Showing the final video, the mask, and the coloured mask       
     try:
@@ -161,15 +95,6 @@ while (True):
     #Try|Except block because an error crashed my laptop, so I'm saving you
     except:
         print("ERROR")
-    '''
-    else:
-        try:
-            cv2.imshow("Left",left)
-            cv2.imshow("Right",right)
-        except:
-            pass
-    '''
-
     #Condition for Exiting Video on pressing the 'Q' Key
     if cv2.waitKey(1) & 0xFF == ord('q'): 
         vid.release()
